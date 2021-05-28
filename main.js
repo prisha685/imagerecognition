4beecccb22ad022fa561551c2765bbcfaad90080
prisha685 <77448105+prisha685@users.noreply.github.com>
2021-05-28 21:31:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ function take_picture(){
 }
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/dbNqfNIAT/',modelLoaded);
 console.log('ml5 version', ml5.version);
+function identify() {
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResults);
+}
+function gotResults(error,results) {
+    if(error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_familymember_name").innerHTML=results[0].label;
+        document.getElementById("result_accuracy_name").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
